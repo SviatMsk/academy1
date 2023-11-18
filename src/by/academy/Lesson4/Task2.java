20bf7213c3e380b8f93615eeb080fc892e06a282
WC <slava.sviat2023@gmail.com>
2023-11-18 14:32:14 +0300

    +

---
+++ src/by/academy/Lesson4/Task2.java
@@ -0,0 +1,14 @@
+package by.academy.Lesson4;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("b");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            System.out.println(i);
+    }
+}
